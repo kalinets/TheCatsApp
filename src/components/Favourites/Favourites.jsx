caee32349f51065be3a1b14bd3d4cd4b61744a76
Uladzimir Kalinets <ukalinets@gmail.com>
2019-06-15 11:10:13 +0300

    favorites component refactor

--- src/components/Favourites/Favourites.jsx
+++ src/components/Favourites/Favourites.jsx
@@ -3,8 +3,9 @@ import { FAVOURITES_URL, API_KEY } from '$constants'
 
 class Favourites extends Component {
   state = {
-    favourites: [],
+    favourites: null,
     loading: true,
+    error: null,
   }
   componentDidMount() {
     this.getFavs()
@@ -26,29 +27,49 @@ class Favourites extends Component {
         this.setState({ favourites: filteredCats, loading: false })
       }
     } catch (error) {
+      this.setState({ error, loading: false })
       throw new Error(error)
     }
   }
 
-  removeFavorite = async id => {
+  addFavouriteBack = async () => {
     try {
-      const res = await fetch(FAVOURITES_URL + '/' + id, {
-        method: 'DELETE',
-        headers: {
-          'x-api-key': API_KEY,
-        },
-      })
-      if (res.ok) {
-        alert('removed')
-        this.getFavs()
-      }
+      console.log('added back')
+      // const res = await fetch(FAVOURITES_URL, {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'x-api-key': API_KEY,
+      //   },
+      //   body: JSON.stringify({ image_id: this.state.selectedCatId, sub_id: 'abramov88' }),
+      // })
+      // if (res.ok) {
+      //   alert('added')
+      // }
     } catch (error) {
       throw new Error(error)
     }
   }
 
+  removeFavorite = async (favId, catId) => {
+    console.log(favId, catId)
+    // try {
+    //   const res = await fetch(FAVOURITES_URL + '/' + favId, {
+    //     method: 'DELETE',
+    //     headers: {
+    //       'x-api-key': API_KEY,
+    //     },
+    //   })
+    //   if (res.ok) {
+    //     alert('removed')
+    //   }
+    // } catch (error) {
+    //   throw new Error(error)
+    // }
+  }
+
   render() {
-    const { favourites, loading } = this.state
+    const { favourites, loading, error } = this.state
     return (
       <>
         <h2>Favourites</h2>
@@ -56,16 +77,21 @@ class Favourites extends Component {
           <h4>Loading...</h4>
         ) : (
           <div>
-            {!favourites.length
-              ? 'You have no favourite cat images'
-              : favourites.map(cat => (
-                  <li key={cat.id}>
-                    {cat.image.url}{' '}
-                    <button onClick={() => this.removeFavorite(cat.id)}>Remove</button>
-                  </li>
-                ))
+            {favourites &&
+              (!favourites.length
+                ? 'You have no favourite cat images'
+                : favourites.map(cat => (
+                    <li key={cat.id}>
+                      {cat.image.url}{' '}
+                      <button onClick={() => this.removeFavorite(cat.id, cat.image_id)}>
+                        Remove
+                      </button>
+                      {/* <button>Add back to favorites</button> */}
+                    </li>
+                  )))
             //: favourites.map(cat => <img key={cat.id} src={cat.image.url} alt="cat" />)
             }
+            {error && <p>{error.message}</p>}
           </div>
         )}
       </>
